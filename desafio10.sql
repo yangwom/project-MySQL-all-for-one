dac947cb5b21bda4bf94e07a657e9dec7a6caf43
yang vieira <yangflix3@gmail.com>
2022-04-06 00:42:35 -0400

    desafio10

--- desafio10.sql
+++ desafio10.sql
@@ -1,1 +1,1 @@
-SELECT created_by FROM northwind.purchase_orders WHERE created_by >= 3  ORDER BY id ASC, created_by DESC;
+SELECT * FROM northwind.purchase_orders WHERE created_by >= 3 ORDER BY created_by DESC ;
--- desafio18.sql
+++ desafio18.sql
@@ -1,2 +1,2 @@
 SELECT supplier_id, status_id FROM purchase_orders
-WHERE supplier_id = 3 AND status_id = 2;
+WHERE supplier_id = 3 and status_id = 2;
